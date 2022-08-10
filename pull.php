fc239694f5050f30fd4fa467117bea4e9e4d17db
Mario Golub <94420233+radicwl@users.noreply.github.com>
2022-08-09 21:40:02 -0400

    Add files via upload

---
+++ pull.php
@@ -0,0 +1,10 @@
+<?php
+
+//for webhooks
+// Use in the “Post-Receive URLs” section of your GitHub repo.
+
+if ( $_POST["payload"] ) {
+	shell_exec( "cd /var/www/ctf && git reset --hard HEAD && git pull" );
+}
+
+?>
