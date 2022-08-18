cbe2cd56290b9215f27be69e4243bf4b6906ac7c
Mario Golub <94420233+radicwl@users.noreply.github.com>
2022-08-18 05:29:40 -0400

    Update pull.php

--- pull.php
+++ pull.php
@@ -1,10 +1,9 @@
 <?php
 
-//for webhooks
-// Use in the “Post-Receive URLs” section of your GitHub repo.
+// Use in the "Post-Receive URLs" section of your GitHub repo.
 
-if ( $_POST["payload"] ) {
-	shell_exec( "cd /var/www/ctf && git reset --hard HEAD && git pull" );
+if ( $_POST['payload'] ) {
+  shell_exec( 'cd /srv/www/git-repo/ && git reset --hard HEAD && git pull' );
 }
 
-?>
+?>hi
